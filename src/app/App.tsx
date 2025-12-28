67ac356d655cf0ac18d1857bc1f8ce5a98183d0f
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-12-28 15:53:10 +0200

    feat(ui): add Main Window

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from '@/components/ui/button';
+import { MainWindow } from '@/components/MainWindow/MainWindow';
 
 function App() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-slate-50 gap-4">
-      <h1 className="text-4xl font-bold text-slate-900">TeamChallenge Chat</h1>
-      <p className="text-slate-500">Frontend setup is complete!</p>
-      <Button>Click me (Shadcn)</Button>
-    </div>
+    // <div className="flex h-screen w-full flex-col items-center justify-center bg-slate-50 gap-4">
+    //   <h1 className="text-4xl font-bold text-slate-900">TeamChallenge Chat</h1>
+    //   <p className="text-slate-500">Frontend setup is complete!</p>
+    //   <Button>Click me (Shadcn)</Button>
+    // </div>
+    <MainWindow />
   );
 }
 
