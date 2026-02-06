a41acbb862169306a2f874cc34f1c7ccaa760f5b
Dennis Dovziy <1044442@gmail.com>
2026-02-06 20:59:30 +0200

    revert(ui): MainWindow temporary inputs deleted (QTAL-139)

--- src/components/MainWindow/MainWindow.tsx
+++ src/components/MainWindow/MainWindow.tsx
@@ -1,16 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import { Button } from '../ui/button';
 import { SocialAuth } from '@/modules/auth/components/SocialAuth';
-import { InputField } from '@components/ui';
 
 // interface MainWindowProps {
 //   setOpenSingUp: (open: boolean) => void;
 // }
 
 export const MainWindow = () => {
-  const onChange = (value: string) => {
-    console.log(value);
-  };
   return (
     <section className="item-center flex h-screen flex-col justify-center">
       <div className="flex flex-1 flex-col items-center justify-center">
@@ -36,40 +32,6 @@ export const MainWindow = () => {
             <Button variant="ghost">Guest Entry</Button>
           </NavLink>
         </div>
-
-        <InputField
-          id="email"
-          onChange={onChange}
-          fieldType="email"
-          placeholder="Email"
-          isError={true}
-          errorMessage={'Invalid email address'}
-        />
-
-        <InputField
-          id="password"
-          onChange={onChange}
-          fieldType="password"
-          placeholder="Password"
-          label={'Password'}
-        />
-
-        <InputField
-          id="bio"
-          onChange={onChange}
-          fieldType="textarea"
-          placeholder="Enter your bio"
-          label={'Bio'}
-        />
-
-        <InputField
-          id="search"
-          onChange={onChange}
-          fieldType="search"
-          placeholder="Search..."
-          label={'Search'}
-        />
-
         <p className="mt-[14px] text-center text-[12px] font-medium leading-[100%] text-[#A3A3A3]">
           By continuing, you agree to our Terms of Service and Privacy Policy
         </p>
